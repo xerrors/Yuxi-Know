c7a8d0030ca03cc7b978b22b27f86b7b0c4c4c8e
Wenjie Zhang <xerrors@163.com>
2025-09-04 01:22:37 +0800

    refactor(models): 导入模型确保表创建时包含所有模型 #258
    确保在创建表之前所有模型都被正确导入并注册到Base.metadata，以便DBManager.create_tables()能够创建包括thread在内的所有表

--- server/models/__init__.py
+++ server/models/__init__.py
@@ -1,3 +1,9 @@
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
+
+# 确保在创建表之前，所有模型都已被导入并注册到 Base.metadata
+# 这样在 DBManager.create_tables() 执行时，可以创建包括 thread 在内的所有表
+from server.models.user_model import User, OperationLog  # noqa: E402, F401
+from server.models.thread_model import Thread  # noqa: E402, F401
+from server.models.kb_models import KnowledgeDatabase, KnowledgeFile, KnowledgeNode  # noqa: E402, F401
